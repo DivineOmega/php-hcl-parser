014d0dc188b353d8ff247558939a2d55f5621ce8
Lior Broshi <lior@binternet.co.il>
2018-06-14 14:23:53 +0300

    PHPDoc

--- src/HCLParser.php
+++ src/HCLParser.php
@@ -2,15 +2,29 @@
 
 namespace DivineOmega\HCLParser;
 
+/**
+ * Class HCLParser
+ * @package DivineOmega\HCLParser
+ */
 class HCLParser
 {
+    /**
+     * @var
+     */
     private $hcl;
 
+    /**
+     * HCLParser constructor.
+     * @param $hcl
+     */
     public function __construct($hcl)
     {
         $this->hcl = $hcl;
     }
 
+    /**
+     * @return string
+     */
     private function getJSONString()
     {
         $command = __DIR__.'/../bin/json2hcl_v0.0.6_linux_amd64 --reverse <<\'EOF\''.PHP_EOL.$this->hcl.PHP_EOL.'EOF';
@@ -20,6 +34,9 @@ class HCLParser
         return implode(PHP_EOL, $lines);
     }
 
+    /**
+     * @return mixed
+     */
     public function parse()
     {
         return json_decode($this->getJSONString());
