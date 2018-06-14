30c23e61eb2ae87ec8f983c4e8452abb8d6f2f7a
Lior Broshi <lior@binternet.co.il>
2018-06-14 14:43:27 +0300

    Added support for multiple operating systems and architecture - macOS / Windows

--- src/HCLParser.php
+++ src/HCLParser.php
@@ -27,7 +27,7 @@ class HCLParser
      */
     private function getJSONString()
     {
-        $command = __DIR__.'/../bin/json2hcl_v0.0.6_linux_amd64 --reverse <<\'EOF\''.PHP_EOL.$this->hcl.PHP_EOL.'EOF';
+        $command = __DIR__.'/../bin/' . Installer::getBinaryFilename() . ' --reverse <<\'EOF\''.PHP_EOL.$this->hcl.PHP_EOL.'EOF';
 
         exec($command, $lines);
 
--- src/Installer.php
+++ src/Installer.php
@@ -2,20 +2,63 @@
 
 namespace DivineOmega\HCLParser;
 
+/**
+ * Class Installer
+ * @package DivineOmega\HCLParser
+ */
 abstract class Installer
 {
+    /**
+     * json2hcl Version we want to use
+     * @see https://github.com/kvz/json2hcl/releases
+     */
+    const JSON2HCL_VERSION = '0.0.6';
+
+    /**
+     * Returns the correct binary filename according to the Operating System and Architecture
+     */
+    public static function getBinaryFilename()
+    {
+        $architecture = 'amd64';
+
+        # Switch architecture if needed
+        if (2147483647 == PHP_INT_MAX) {
+            $architecture = '386';
+        }
+
+        # Default OS
+        $osString = 'linux';
+
+        switch (TRUE) {
+            case stristr(PHP_OS, 'DAR'):
+                $osString = 'darwin';
+                break;
+            case stristr(PHP_OS, 'WIN'):
+                $osString = 'windows';
+                break;
+        }
+
+        return sprintf('json2hcl_v%s_%s_%s',self::JSON2HCL_VERSION, $osString, $architecture);
+    }
+
     public static function installBinaries()
     {
-        $binaryUrls = ['https://github.com/kvz/json2hcl/releases/download/v0.0.6/json2hcl_v0.0.6_linux_amd64'];
+        $binaryUrls = [
+            sprintf('https://github.com/kvz/json2hcl/releases/download/v%s/%s', self::JSON2HCL_VERSION, self::getBinaryFilename() )
+        ];
 
         foreach ($binaryUrls as $binaryUrl) {
-            $destination = __DIR__.'/../bin/'.basename($binaryUrl);
+            $destination = __DIR__ . '/../bin/' . basename($binaryUrl);
 
+            # Skip if file exists
             if (file_exists($destination)) {
                 continue;
             }
 
+            # Download
             file_put_contents($destination, file_get_contents($binaryUrl));
+
+            # Make executable
             chmod($destination, 0755);
         }
     }
